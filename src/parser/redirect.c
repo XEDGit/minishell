9da5b004ba34070cc979dd6ba4670e4bf64b9c08
NicoMolinelli <nic.molinelli@gmail.com>
2022-05-24 23:17:00 +0200

    Get the right name of the file for open()

--- src/parser/redirect.c
+++ src/parser/redirect.c
@@ -1,5 +1,31 @@
 #include <parser.h>
 
+// Retrive and expand the redirect word(filename)
+char    *get_filename(char **input)
+{
+    char    *tmp;
+    char    *name;
+	int		len;
+
+	len = 0;
+    while (ft_isspace(**input))
+        (*input)++;
+	tmp = *input;
+	while (*tmp && !ft_isspace(*tmp++))// check other metachars
+		len++;
+	tmp = malloc(sizeof(char) * (++len));
+	if (!tmp)
+		return ((char *) error_msg("Malloc failed"));
+	ft_strlcpy(tmp, *input, len);
+	*input += len - 1;
+	name = remove_quotes(tmp);
+	free(tmp);
+	if (!name)
+		return ((char *) error_msg("Remove quotes failed"));
+	printf("Filename: %s\n", name);
+    return (name);
+}
+
 char	*set_redirects(char *table, t_cmd *cmd, int **docs)
 {
 	int		open;
