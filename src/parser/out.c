db30bbf5138c89c82b1de9655edf88f82aabb9ef
NicoMolinelli <nic.molinelli@gmail.com>
2022-06-06 12:10:39 +0200

    Merge right and append funcs + improve out code

--- src/parser/out.c
+++ src/parser/out.c
@@ -1,55 +1,40 @@
 #include <parser.h>
 
-// TODO: merge right and append funcs
-static int right_rdrt(char *file, t_cmd *cmd)
+static int	set_output_fd(t_cmd *cmd, char *file, int open_flags)
 {
-    if (cmd->redirects[1] != STDOUT_FILENO)
+	if (cmd->redirects[1] != STDOUT_FILENO)
 	{
 		if (close(cmd->redirects[1]) == ERROR)
 			return (error_int("Close error", 0));
 	}
-    cmd->redirects[1] = open(file, O_WRONLY | O_CREAT | O_TRUNC, MODE);
-    free(file);
-    if (cmd->redirects[1] == ERROR)
-        return (error_int("Open error", 0)); // handle precise error
-    return (1);
-}
-
-static int append(char *file, t_cmd *cmd)
-{
-    if (cmd->redirects[1] != STDOUT_FILENO)
-    {
-		if (close(cmd->redirects[1]) == ERROR)
-			return (error_int("Close error", 0));
-	}
-    cmd->redirects[1] = open(file, O_WRONLY | O_CREAT | O_APPEND, MODE);
-    free(file);
-    if (cmd->redirects[1] == ERROR)
-        return (error_int("Open error", 0)); // handle precise error
-    return (1);
+	cmd->redirects[1] = open(file, open_flags, MODE);
+	free(file);
+	if (cmd->redirects[1] == ERROR)
+		return (error_int("Open error", 0)); // handle precise error
+	return (1);
 }
 
 void	*out_redirect(char **table, t_cmd *cmd)
 {
+	int		flags;
 	char	*file;
 
 	(*table)++;
 	if (**table == RIGHT_REDIRECT)
 	{
 		(*table)++;
-		file = get_filename(table);
-		if (!file)
-			return (error_msg("File name failed")); // error
-		if (!append(file, cmd))
-			return (error_msg("Append failed")); // append error
+		if (**table == LEFT_REDIRECT || **table == RIGHT_REDIRECT)
+			return (0);
+		flags = O_WRONLY | O_CREAT | O_APPEND;
 	}
 	else
 	{
-		file = get_filename(table);
-		if (!file)
-			return (error_msg("File name failed")); // error
-		if (!right_rdrt(file, cmd))
-			return (error_msg("Right redirect failed")); // right redirect error
+		if (**table == LEFT_REDIRECT)
+			return (0);
+		flags = O_WRONLY | O_CREAT | O_TRUNC;
 	}
-	return ((void *) 1);
+	file = get_filename(table);
+	if (file && set_output_fd(cmd, file, flags))
+		return ((void *) 1);
+	return (error_msg("Rx redirect failed"));
 }
