a790da4064e8232778480c60ea088f749221913f
XEDGit <muzio200@gmail.com>
2022-08-04 22:40:03 +0200

    Updated tester with argv

--- tester.py
+++ tester.py
@@ -1,7 +1,13 @@
 import subprocess
+import sys
 
-fd = open("TODO", "r")
+file = "TODO"
+if len(sys.argv) > 1:
+	file = str(sys.argv[1])
+fd = open(file, "r")
 tests = fd.readlines()
 p = subprocess.Popen("./minishell", stdin=subprocess.PIPE)
 for test in tests:
 	p.stdin.write(test.encode())
+p.stdin.close()
+p.terminate()
