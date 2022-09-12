6d202695626dcf972ac060fa8bbd4a7978d21073
XEDGit <muzio200@gmail.com>
2022-09-12 15:04:58 +0200

    tester updated

--- tester.py
+++ tester.py
@@ -12,20 +12,41 @@ def send_cmd(prog):
 	p.wait()
 	return p.returncode, p_res.decode().rstrip("\n")
 
+def col(str, col):
+	return f"\033[{col}m{str}\033[0m"
+
 file = "tests"
+single = -1
+noout = True
+sep = "\n\033[1m________________________________________________________________\033[0m\n"
 if len(sys.argv) > 1:
-	file = str(sys.argv[1])
+	if str(sys.argv[1]) == "-o":
+		noout = False
+	else:
+		single = int(sys.argv[1]) - 1
+if len(sys.argv) > 2:
+	if str(sys.argv[2]) == "-o":
+		noout = False
+	else:
+		file = str(sys.argv[2])
+if noout and len(sys.argv) > 3:
+	file = str(sys.argv[3])
 fd = open(file, "r")
 tests = fd.readlines()
 for i, test in enumerate(tests):
+	if single != -1 and i != single:
+		continue
 	mini_code, mini_res = send_cmd("./minishell")
 	bash_code, bash_res = send_cmd("bash")
-	diff = "\033[31mNo\033[0m"
+	diff = col("Fail", "31")
 	if mini_res == bash_res:
-		diff = "\033[32mYes!\033[0m"
+		diff = col("Success!", "32")
 	if mini_code != bash_code:
-		diff = "\033[33mWrong exit code\033[0m"
+		diff = col("Wrong exit code", "33")
 	test = test.rstrip("\n")
-	out = f"ms:'{mini_res}' {mini_code} - bash:'{bash_res}' {bash_code} - Equal: {diff}"
-	print("Line {:d}:{:<30.30s}	{:>75s}".format(i, test, out))
+	if noout:
+		out = f"{col(f'Line {i + 1}: ', '1;31')}{test} {diff}{sep}"
+	else:
+		out = f"{col(f'Line {i + 1}: ', '1;31')}{test}\n{col('minishell: ', '31')}\n'{mini_res}'\n{col('exit code: ', '31')}{mini_code}\n{col('bash: ', '31')}\n'{bash_res}'\n{col('exit code: ', '31')}{bash_code}\n{diff}{sep}"
+	print(out)
 	
