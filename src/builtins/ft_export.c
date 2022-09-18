0675920d4962ec7b9da2cba8b764544300f50c70
NicoMolinelli <nic.molinelli@gmail.com>
2022-09-18 17:05:08 +0200

    Export + buff_copy

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -27,50 +27,62 @@ int	get_arr_length(char **arr)
 	return (i);
 }
 
-// Mi cacciano da codam
-// int	print_p(t_env *env)
-// {
-// 	int		i;
-// 	int		s;
-// 	int		m;
-// 	int		length;
-// 	size_t	pos;
-// 	t_buffvar	*cp;
+int	print_lowest_ascii(char **mem)
+{
+	int		i;
+	int		s;
+	int		m;
+	char	*pos;
 
-// 	cp = buff_create(env->envp->mem);
-// 	if (!cp)
-// 		return (137);
-// 	length = get_arr_length(env->envp->mem);
-// 	while (length--)
-// 	{
-// 		m = 0;
-// 		i = 1;
-// 		s = 0;
-// 		while (env->envp->mem[i])
-// 		{
-// 			while (env->envp->mem[i][s] && env->envp->mem[m][s] && env->envp->mem[i][s] == env->envp->mem[m][s])
-// 				s++;
-// 			if (env->envp->mem[i][s] < env->envp->mem[m][s])
-// 				m = i;
-// 			i++;
-// 		}
-// 		pos = ft_strchr(env->envp->mem[m], '=') -  env->envp->mem[m];
-// 		env->envp->mem[m][pos] = 0;
-// 		ft_printf("%s%s=\"%s\"\n", EXPDEC, env->envp->mem[m], env->envp->mem[m] + pos + 1);
-// 		env_remove(env, env->envp->mem[m], 1);
-// 	}
-// 	buff_free(env->envp);
-// 	env->envp = cp;
-// 	return (0);
-// }
+	m = 0;
+	i = 1;
+	while (mem[i])
+	{
+		s = 0;
+		while (mem[i][s] && mem[m][s] && mem[i][s] == mem[m][s])
+			s++;
+		if (mem[i][s] < mem[m][s])
+			m = i;
+		i++;
+	}
+	pos = ft_strchr(mem[m], '=');
+	if (!pos)
+		ft_printf("%s%s\n", EXPDEC, mem[m]);
+	else
+	{
+		*pos++ = 0;
+		ft_printf("%s%s=\"%s\"\n", EXPDEC, mem[m], pos);
+	}
+	return (m);
+}
+
+int	print_p(t_env *env)
+{
+	int			length;
+	int			index;
+	t_buffvar	*cp;
+
+	cp = buff_create(env->envp->mem);
+	if (!cp)
+		return (137);
+	length = get_arr_length(env->envp->mem);
+	while (length--)
+	{
+		index = print_lowest_ascii(env->envp->mem);
+		env_remove(env, env->envp->mem[index], 1);
+	}
+	buff_free(env->envp);
+	env->envp = cp;
+	return (0);
+}
 
 int	ft_export(char **args, t_env *env)
 {
 	int		index;
 	char	*to_add;
 
-	// if (!*(++args))
-	// 	return (print_p(env));
+	if (!*(++args))
+		return (print_p(env));
 	while (args && *args)
 	{
 		index = buff_contains(env->envl, *args, 0);
