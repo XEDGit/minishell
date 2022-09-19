fc241fdff9d0b8faa306355391bb5852f010e2be
NicoMolinelli <nic.molinelli@gmail.com>
2022-09-19 11:39:37 +0200

    Fix ft_env for empty vars

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -14,10 +14,16 @@
 
 int	ft_env(char **args, t_env *env)
 {
+	int		i;
 	char	**envp;
 
+	i = 0;
 	envp = env->envp->mem;
-	while (envp && *envp && ft_strchr(*envp, '='))
-		ft_printf("%s\n", *envp++);
+	while (envp[i])
+	{
+		if (ft_strchr(envp[i], '='))
+			ft_printf("%s\n", envp[i]);
+		i++;
+	}
 	return (0);
 }
