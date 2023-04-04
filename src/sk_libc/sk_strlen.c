37571b70e2368ffffc03ca621db279412faafb8c
XEDGit <muzio200@gmail.com>
2023-04-04 20:47:44 +0200

    Fixed subshell expansion crashing with condition or pipe inside

--- src/sk_libc/sk_strlen.c
+++ src/sk_libc/sk_strlen.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   sk_strlen.c                                        :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: lmuzio <lmuzio@student.42.fr>                +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2022/05/10 20:53:46 by lmuzio        #+#    #+#                 */
-/*   Updated: 2022/06/08 23:56:10 by lmuzio        ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   sk_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmuzio <lmuzio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 20:53:46 by lmuzio            #+#    #+#             */
+/*   Updated: 2023/04/04 20:46:44 by lmuzio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,17 @@ size_t	sk_strclen(const char *start, const char *end)
 	return (cont);
 }
 
+size_t	skip_parentheses(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '(')
+		while (str[i] && str[i] != ')')
+			i++;
+	return (i);
+}
+
 size_t	sk_strclen_no_quotes(const char *start, const char *end)
 {
 	int			cont;
@@ -55,6 +66,8 @@ size_t	sk_strclen_no_quotes(const char *start, const char *end)
 		end = temp;
 		cont += skip_quotes((char *)start);
 		start += skip_quotes((char *)start);
+		cont += skip_parentheses((char *)start);
+		start += skip_parentheses((char *)start);
 		while (*end)
 			if (*start == *end++)
 				return (cont);
