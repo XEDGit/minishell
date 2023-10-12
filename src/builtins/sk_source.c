198eadc9ce09d3167421a176aeef9cc1686dd5b5
XED <xed@xed.com>
2023-10-12 15:19:50 +0200

    Fixing access/open time window

--- src/builtins/sk_source.c
+++ src/builtins/sk_source.c
@@ -9,34 +9,28 @@ int sk_source(t_cmd *cmd, t_data *data)
     if (!cmd->args[1])
         return (error_int("filename argument is required", "source", -1, 1));
     path = cmd->args[1];
-    if (!access(path, F_OK) && !access(path, R_OK))
-	{
-		int fd = open(path, O_RDONLY);
-		if (fd == -1)
-			return (1);
-		read_ret = 1;
-		while (read_ret)
-		{
-			read_ret = read(fd, &buff, 1000);
-			if (read_ret >= 0)
-				buff[read_ret] = 0;
-			else
-				continue;
-			buff_ptr = &buff[0];
-			while ((path = sk_strchr(buff_ptr, '\n')))
-			{
-				*path = 0;
-				lexer(buff_ptr, data->env, data->aliases, false);
-				buff_ptr = path + sizeof(char);
-			}
-			if (buff_ptr != &buff[read_ret])
-				lexer(buff_ptr, data->env, data->aliases, false);
-		}
-		if (read_ret == -1)
-			err = error_int("error reading file", "source", -1, 1);
-		close(fd);
-	}
-    else
+	int fd = open(path, O_RDONLY);
+    if (fd == -1)
         err = error_int("file not found or not readable", "source", -1, 1);
+	read_ret = 1;
+	while (read_ret > 0)
+	{
+		read_ret = read(fd, &buff, 1000);
+		if (read_ret < 0)
+			continue;
+		buff[read_ret] = 0;
+		buff_ptr = &buff[0];
+		while ((path = sk_strchr(buff_ptr, '\n')))
+		{
+			*path = 0;
+			lexer(buff_ptr, data->env, data->aliases, false);
+			buff_ptr = path + sizeof(char);
+		}
+		if (buff_ptr != &buff[read_ret])
+			lexer(buff_ptr, data->env, data->aliases, false);
+	}
+	if (read_ret == -1)
+		err = error_int("error reading file", "source", -1, 1);
+	close(fd);
 	return (err);
 }
